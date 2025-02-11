667f3faca8bd484d2bb3b8a22e918feb8c1c9dea
adeymoe <66586650+adeymoe@users.noreply.github.com>
2025-02-11 11:26:00 -0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
@@ -11,3 +10,4 @@ export default defineConfig({
     strictPort: true,
     allowedHosts: ["post-checkapp.onrender.com"]
   }
+})
